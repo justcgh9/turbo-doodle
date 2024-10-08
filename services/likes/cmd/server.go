272a49958c6503ff4f06e5801f5bb90ab9c3b3b0
maksim <make.oynoshev@gmail.com>
2024-10-08 16:02:23 +0300

    disable cores and adjust likes struct according to db schema

--- services/likes/cmd/server.go
+++ services/likes/cmd/server.go
@@ -12,12 +12,12 @@ import (
 
 type LikeReq struct {
 	Username string `json:"username"`
-	PostId   int    `json:"postId"`
+	MessageId   int    `json:"messageId"`
 }
 
 type LikeRes struct {
 	Username string `json:"username"`
-	PostId   int    `json:"postId"`
+	MessageId   int    `json:"messageId"`
 }
 
 func ErrorResponse(w http.ResponseWriter, code int, msg string) {
@@ -26,7 +26,7 @@ func ErrorResponse(w http.ResponseWriter, code int, msg string) {
 }
 
 func main() {
-	db, err := sql.Open("sqlite3", "../../storage/database.db")
+	db, err := sql.Open("sqlite3", "../../storage/db.sql")
 	if err != nil {
 		slog.Error("main:", "error", err)
 		return
@@ -42,22 +42,26 @@ func main() {
 		var req LikeReq
 		err := json.NewDecoder(r.Body).Decode(&req)
 		if err != nil {
+			slog.Error("main:", "error", err)
 			ErrorResponse(w, http.StatusBadRequest, "invalid body")
 			return
 		}
 
-		_, err = db.Exec("INSERT INTO like (username, post_id) VALUES ($1, $2)", req.Username, req.PostId)
+		_, err = db.Exec("INSERT INTO likes (username, message_id) VALUES ($1, $2)", req.Username, req.MessageId)
 		if err != nil {
+			slog.Error("main:", "error", err)
 			ErrorResponse(w, http.StatusInternalServerError, "failed create like")
 			return
 		}
 
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.WriteHeader(http.StatusNoContent)
 	})
 
 	http.HandleFunc("GET /likes", func(w http.ResponseWriter, r *http.Request) {
-		rows, err := db.Query("SELECT username, post_id FROM like")
+		rows, err := db.Query("SELECT username, message_id FROM likes")
 		if err != nil {
+			slog.Error("main:", "error", err)
 			ErrorResponse(w, http.StatusInternalServerError, "failed get likes")
 			return
 		}
@@ -65,8 +69,9 @@ func main() {
 		likes := make([]LikeRes, 0, 10)
 		for rows.Next() {
 			var like LikeRes
-			err = rows.Scan(&like.Username, &like.PostId)
+			err = rows.Scan(&like.Username, &like.MessageId)
 			if err != nil {
+				slog.Error("main:", "error", err)
 				ErrorResponse(w, http.StatusInternalServerError, "failed get likes")
 				return
 			}
@@ -75,9 +80,11 @@ func main() {
 
 		res, err := json.Marshal(likes)
 		if err != nil {
+			slog.Error("main:", "error", err)
 			ErrorResponse(w, http.StatusInternalServerError, "failed get likes")
 			return
 		}
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Write(res)
 	})
 
