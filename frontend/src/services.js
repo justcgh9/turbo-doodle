290cc7031019e695fd3f34c2e40641dfceba4e15
Nikolai Petukhov <n.petukhov@innopolis.university>
2024-10-09 15:58:59 +0300

    have added full versions for each of the posts and started working with likes

--- frontend/src/services.js
+++ frontend/src/services.js
@@ -1,3 +1,3 @@
-export const LIKES_API = "http://localhost:8080/"
+export const LIKES_API = "http://localhost:8080/likes"
 export const POSTS_API = "http://localhost:8081/posts/"
-export const USERS_API = "http://localhost:8082/users"
+export const USERS_API = "http://localhost:8082/users/"
